fb348f65b6e2d30d2ddcae0894cea8f421e14371
tjrileywisc <timothyjohnriley@gmail.com>
2021-08-06 15:54:49 -0400

    Fix square ids Add next step button

--- src/index.js
+++ src/index.js
@@ -2,33 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function NextStep(props) {
+  const str = `Run step ${props}`;
+  return <button>{str}</button>;
+}
+
 function Square(props) {
   return <button className="square">{props.value}</button>;
 }
 
 class MessageBoard extends React.Component {
-
   renderSquare(i) {
     return <Square value={this.props.squares[i]} key={i} />;
   }
 
   render() {
-
     const rowNum = Math.sqrt(this.props.squares.length);
     const colNum = rowNum;
 
     let domRows = [];
 
-    for(let i = 0; i < rowNum; i++) {
+    let squareKey = 0;
+
+    for (let i = 0; i < rowNum; i++) {
       let thisRow = [];
-      for(let j = 0; j < colNum; j++) {
-        thisRow.push(this.renderSquare(i + j));
+      for (let j = 0; j < colNum; j++) {
+        thisRow.push(this.renderSquare(squareKey));
+        squareKey += 1;
       }
-      domRows.push(<div className="board-row" key={i}>{thisRow}</div>);
+      domRows.push(
+        <div className="board-row" key={i}>
+          {thisRow}
+        </div>
+      );
     }
-    return (
-      <div>{domRows}</div>
-    );
+    return <div>{domRows}</div>;
   }
 }
 
@@ -39,7 +47,8 @@ class Hamming extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares : Array(16).fill(null),
+      squares: Array(16).fill(null),
+      step: 1,
     };
   }
 
@@ -47,11 +56,10 @@ class Hamming extends React.Component {
     return (
       <div className="hamming">
         <div className="hamming-board">
-          <MessageBoard
-            squares={this.state.squares}
-          />
+          <MessageBoard squares={this.state.squares} />
         </div>
         <div className="hamming-info">
+          <div>{NextStep(this.state.step)}</div>
         </div>
       </div>
     );
